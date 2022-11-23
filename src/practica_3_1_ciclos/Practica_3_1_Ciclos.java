fc09fecf7f70eb2c101b23b1ba505ee2e6ffe751
Rafael De Jesus Martinez Ovando <94251980+Rafsterman06@users.noreply.github.com>
2022-11-22 20:22:31 -0700

    Practica de ciclos

---
+++ src/practica_3_1_ciclos/Practica_3_1_Ciclos.java
@@ -0,0 +1,26 @@
+package practica_3_1_ciclos;
+/**
+ *
+ * @author rafael
+ */
+public class Practica_3_1_Ciclos {
+    public static void main(String[] args) {
+        System.out.println("Ciclo for");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Rafael De Jesus");
+            
+        }
+        System.out.println("Ciclo while");
+        int j=0;
+        while (j<5) {            
+            System.out.println("Martinez");
+            j++;
+        }
+        System.out.println("Ciclo do while");
+        int k=0;
+        do {  
+            System.out.println("Ovando");
+            k++;
+        } while (k<5);
+    }   
+}
